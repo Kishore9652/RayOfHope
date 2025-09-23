d060924e5ed9b765f5f97336f5acbd0866108cbe
Kishore <ekishore250@gmail.com>
2025-09-23 17:04:04 +0530

    Add TestDbConnection and fix DB config for Railway

---
+++ src/main/java/com/rehab/loginbackend/TestDbConnection.java
@@ -0,0 +1,29 @@
+package com.rehab.loginbackend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+@Component
+public abstract class TestDbConnection implements CommandLineRunner {
+
+	
+	@Override
+    public void run(String... args) throws Exception {
+        // Use your public Railway MySQL values here
+        String url = "jdbc:mysql://mysql.railway.internal:3306/railway";
+        String username = "<your-username>";
+        String password = "<your-password>";
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            System.out.println("✅ DB Connection Successful!");
+        } catch (Exception e) {
+            System.out.println("❌ DB Connection Failed!");
+            e.printStackTrace();
+        }
+    }
+	
+}
